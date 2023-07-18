6714dec19519ee3321f5b801f146df3cd9da6e4e
Kim Bum June <bjbj6363@gmail.com>
2023-07-18 16:48:56 +0900

    feat: add mastodon service

---
+++ src/activityPub/MastodonService.types.ts
@@ -0,0 +1,14 @@
+export type Application = {
+  name: string;
+  website?: string;
+  vapid_key: string;
+  client_id?: string;
+  client_secret?: string;
+};
+
+export type Token = {
+  access_token: string;
+  token_type: string;
+  scope: string;
+  created_at: number;
+};
