c3e57567ebfa3ac86f1bf3bbe722ff6e07a021bf
bskim <bskim2@rsupport.com>
2023-08-09 09:53:20 +0900

    feat: add server name input element

--- src/routes/signin/index.tsx
+++ src/routes/signin/index.tsx
@@ -3,9 +3,14 @@ import MastodonService from '$activityPub/MastodonService';
 import { Application } from '$activityPub/MastodonService.types';
 
 const SignIn = () => {
+  const [serverName, setServerName] = useState('https://mastodon.social');
   const [service] = useState(() => new MastodonService({ baseUrl: 'https://mastodon.social' }));
   const [result, setResult] = useState<Application>(null);
   const [authorizationCode, setAuthorizationCode] = useState<string>(null);
+
+  const handleClickSingInBtn = async () => {
+    //
+  };
 
   useEffect(() => {
     const test = async () => {
@@ -40,6 +45,8 @@ const SignIn = () => {
   return (
     <div>
       <h1>SignIn</h1>
+      <input placeholder="enter server name" value={serverName} onChange={(e) => setServerName(e.target.value)} />
+      <button onClick={handleClickSingInBtn}>Sign in</button>
       <div>code: {authorizationCode}</div>
     </div>
   );
