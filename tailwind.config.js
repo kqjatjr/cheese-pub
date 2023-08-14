e921005da6060929027b36ffee8e4f82fc52d715
김범석 <kqjatjr@gmail.com>
2023-08-14 15:20:07 +0900

    chore: setting tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
   },
   darkMode: 'class',
   plugins: [nextui()],
