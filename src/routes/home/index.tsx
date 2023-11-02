251a419aca99f6e3ac1c983008bedb9db452d444
김범석 <kqjatjr@gmail.com>
2023-11-02 21:37:38 +0900

    feat: 로그인 유무 확인 로직 추가

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -5,22 +5,25 @@ import { accountsAtom } from '$atoms/accounts';
 import { useAtomValue } from 'jotai';
 import Feed from '$routes/home/components/Feed';
 import AddServerFab from '$routes/home/components/AddServerFab';
+import Header from './components/Header';
 
 const Home = () => {
   const navigate = useNavigate();
   const instances = useAtomValue(accountsAtom);
   const isUserLoggedIn = instances.length > 0;
+  const accessToken = sessionStorage.getItem('ACCESS_TOKEN');
 
   useEffect(() => {
-    if (!isUserLoggedIn && instances.length === 0) {
+    if (!accessToken) {
       navigate(RoutePaths.SIGN_IN.HOME);
     }
-  }, [isUserLoggedIn]);
+  }, [accessToken]);
 
   if (!isUserLoggedIn) return null;
 
   return (
     <>
+      <Header title="CHEESE PUB" />
       <div className="flex gap-2 justify-items-start overflow-hidden h-screen">
         {instances.map((instance) => (
           <div className="w-2/6  h-ful" key={instance.id}>
