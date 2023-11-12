2a1200011322815fee9361140f1f65db817a4f6e
김범석 <kqjatjr@gmail.com>
2023-11-12 15:48:44 +0900

    feat: 계정 아바타 및 정보 받아오기 추가

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,46 @@
 import { Instance } from '$atoms/accounts';
 import { Avatar } from '@nextui-org/react';
-import React, { FC } from 'react';
+import generator from 'megalodon';
+import React, { FC, useEffect, useState } from 'react';
 
 interface ISidebarProps {
   instances: Instance[];
 }
 
 const Sidebar: FC<ISidebarProps> = ({ instances }) => {
-  console.log(instances);
+  const [accountList, setAccountList] = useState<Entity.Account[]>([]);
+
+  const getAccountData = async (instance: Instance) => {
+    const client = generator(instance.type, instance.url, instance.accessToken);
+    const { data } = await client.verifyAccountCredentials();
+    setAccountList((prev) => [...prev, data]);
+  };
+
+  useEffect(() => {
+    instances.forEach((instance) => {
+      getAccountData(instance);
+    });
+  }, [instances]);
+
+  if (accountList.length <= 0) {
+    return (
+      <div className="bg-darkColor w-[58px] flex items-center flex-col gap-[15px] p-[15px]">
+        {instances.map((instance) => {
+          return <Avatar isBordered radius="md" key={instance.id} />;
+        })}
+      </div>
+    );
+  }
+
   return (
     <div className="bg-darkColor w-[58px] flex items-center flex-col gap-[15px] p-[15px]">
-      <Avatar isBordered radius="md" src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
-      <Avatar isBordered radius="md" src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
-      <Avatar isBordered radius="md" src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
+      {accountList.length
+        ? accountList.map((account) => {
+            return <Avatar isBordered radius="md" key={account.id} src={account.avatar} />;
+          })
+        : instances.map((instance) => {
+            return <Avatar isBordered radius="md" key={instance.id} />;
+          })}
     </div>
   );
 };
