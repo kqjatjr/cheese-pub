17814af8d28a8e50231f446966652b934b48ed3b
김범석 <kqjatjr@gmail.com>
2023-11-20 22:37:00 +0900

    feat: file upload 추가

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -10,7 +10,7 @@ import {
   useDisclosure,
 } from '@nextui-org/react';
 import generator from 'megalodon';
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 interface IProps {
   instance?: Instance;
@@ -20,6 +20,7 @@ const Editor = ({ instance }: IProps) => {
   const [textContent, setTextContent] = useState('');
   const client = instance && generator(instance.type, instance.url, instance.accessToken);
   const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure();
+  const [files, setFiles] = useState<string[]>([]);
 
   const onClickPostBtn = async () => {
     if (!textContent.length) {
@@ -27,12 +28,28 @@ const Editor = ({ instance }: IProps) => {
     }
     const result = await client?.postStatus(textContent, {
       scheduled_at: '',
+      media_ids: files.length ? files : [],
     });
     if (result?.status === 200) {
       setTextContent('');
       onClose();
     } else {
       alert(`포스팅에 실패하였습니다.(${result?.status})`);
+    }
+  };
+
+  const handleUploadMediaFile = async (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (file) {
+      try {
+        const result = await client?.uploadMedia(file);
+        result && setFiles((prev) => [...prev, result.data.id]);
+      } catch (error) {
+        console.error('오류:', error);
+      }
+    } else {
+      console.error('파일이 선택되지 않았습니다.');
     }
   };
 
@@ -53,6 +70,7 @@ const Editor = ({ instance }: IProps) => {
             />
           </ModalBody>
           <ModalFooter>
+            <input type="file" onChange={handleUploadMediaFile} />
             <Button onClick={onClickPostBtn}>업로드</Button>
           </ModalFooter>
         </ModalContent>
