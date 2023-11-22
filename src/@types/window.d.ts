7718f760b6b45ba7bc5041a1c59fcf9090ca6397
김범석 <kqjatjr@gmail.com>
2023-11-22 14:56:58 +0900

    feat: 전역 타입 추가

--- src/@types/window.d.ts
+++ src/@types/window.d.ts
@@ -4,4 +4,13 @@ declare global {
   interface Window {
     electron: GetEntryExposedApi;
   }
+  interface Instance {
+    id: string;
+    type: ServiceType;
+    url: string;
+    accessToken: string;
+  }
+  interface IAccount extends Entity.Account {
+    instanceId: string;
+  }
 }
