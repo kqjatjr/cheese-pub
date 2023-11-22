e9567c9f738ddea58a9e153e5fd5131eaead1434
김범석 <kqjatjr@gmail.com>
2023-11-22 14:56:23 +0900

    refactor: 계정 리스트 훅으로 리펙토링

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -6,35 +6,22 @@ import { useAtomValue } from 'jotai';
 import { Instance, accountsAtom } from '$atoms/accounts';
 import Header from './components/Header';
 import Sidebar from '$components/Sidebar';
-import generator, { Entity } from 'megalodon';
 import { Spinner } from '@nextui-org/react';
 import Editor from '$components/Editor';
-
-export interface IAccount extends Entity.Account {
-  instanceId: string;
-}
+import { useAccountList } from '$hooks/useAccountList';
 
 const Home = () => {
   const navigate = useNavigate();
   const instances = useAtomValue(accountsAtom);
   const isUserLoggedIn = instances.length > 0;
-  const [accountList, setAccountList] = useState<IAccount[]>([]);
+  const { accountList } = useAccountList();
   const [focusInstance, setFocusInstance] = useState<Instance>();
-
-  const getAccountData = async (instance: Instance) => {
-    const client = generator(instance.type, instance.url, instance.accessToken);
-    const { data } = await client.verifyAccountCredentials();
-    setAccountList((prev) => [...prev, { ...data, instanceId: instance.id }]);
-  };
 
   useEffect(() => {
     if (!isUserLoggedIn) {
       navigate(RoutePaths.SIGN_IN.HOME);
     }
     setFocusInstance(instances[instances.length - 1]);
-    instances.forEach((instance) => {
-      getAccountData(instance);
-    });
   }, [instances]);
 
   const handleChangeFocusInstance = (id: string) => {
@@ -55,14 +42,14 @@ const Home = () => {
           }
         >
           <Sidebar
-            accountList={accountList}
+            accountList={accountList ?? []}
             focusInstanceId={focusInstance?.id}
             onChangeFocusInstance={handleChangeFocusInstance}
           />
           <div className='flex gap-2 w-full h-full justify-items-start overflow-hidden h-screen" p-[15px]'>
             {instances.map((instance) => (
               <div className="w-2/6  h-ful" key={instance.id}>
-                {accountList.map((account) => {
+                {accountList?.map((account) => {
                   if (account.instanceId === instance.id) {
                     return <div key={account.id}>{account.username}</div>;
                   }
